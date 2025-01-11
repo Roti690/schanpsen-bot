643a58fceaebaffc73041aaadfa11d89e1d7608f
Roti690 <arvaidavid5@gmail.com>
2025-01-11 17:14:46 +0100

    ML-bot + testarena

--- models/Deepbot.py
+++
@@ -1,8 +0,0 @@
-from schnapsen.game import Bot, PlayerPerspective, Move, GameState, GamePlayEngine
-import random
-from typing import Optional
-
-
-class DeepBot(Bot):
-    def __init__(self, name: Optional[str] = None) -> None:
-        super().__init__(name)
